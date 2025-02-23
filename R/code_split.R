be44068ba65746e3a2ec7a1862325fff0703e7fa
微信公众号 RStata <957028646czx@gmail.com>
2025-02-23 16:35:20 +0800

    fixed some errors

--- R/code_split.R
+++ R/code_split.R
@@ -3,9 +3,25 @@
 #' @import stringr
 #' @import dplyr
 #' @importFrom purrr map2
+#' @importFrom readr write_rds
 #' @export
 code_split <- function(file = "main.smd"){
+  # delete pre-files
+  try({
+    fs::dir_delete("asis_blocks")
+    fs::dir_delete("logfile_blocks")
+    fs::dir_delete("picture_blocks")
+    file.remove("temp_smd_dofile.do")
+    file.remove(paste0(tools::file_path_sans_ext(basename(file)), ".Rmd"))
+    file.remove("new_md_content.rds")
+    file.remove("code_blocks.rds")
+  })
   md_content <- readLines(file)
+  md_content %>%
+    as_tibble() %>%
+    mutate(value = paste0("[[ROW_", row_number(), "]]--", value)) %>%
+    pull(value) -> md_content
+
   code_blocks <- str_extract_all(paste(md_content, collapse = "\n"), "```[\\s\\S]*?```")[[1]]
   output_dir <- "logfile_blocks"
   if (!dir.exists(output_dir)) {
@@ -26,13 +42,15 @@ code_split <- function(file = "main.smd"){
            id = paste0("\n\n*- [[CODE_BLOCK_", row_number(), "]]")) %>%
     select(rawid, id, everything()) -> code_blocksdf
   code_blocksdf %>%
-    dplyr::filter(str_detect(value, "\\{stata asis\\}")) %>%
-    mutate(value = str_remove_all(value, "```|\\{stata asis\\}(\\s*)\n")) %>%
-    mutate(res = purrr::map2(rawid, value, ~writeLines(.y, paste0(asis_dir, "/code_block", .x, ".do")))) -> tempres
+    filter(str_detect(value, "\\{stata asis\\}")) %>%
+    mutate(value = str_remove_all(value, "```|\\{stata asis\\}(\\s*)\n"),
+           value = str_remove_all(value, "\\[\\[ROW_(\\d{1,})\\]\\]--")) %>%
+    mutate(res = map2(rawid, value, ~writeLines(.y, paste0(asis_dir, "/code_block", .x, ".do")))) -> tempres
   code_blocksdf %>%
     mutate(value = str_remove_all(value, "```|\\{stata\\}|\\{stata asis\\}"),
-           value = paste0("set linesize 80\ncap graph close\ncap log close\nlog using logfile_blocks/tempsmddoclog", row_number(), ", replace text nomsg\n", value, "\nlog close\ncap graph export picture_blocks/tempsmddocpic", row_number(), ".png, replace width(4800)"),
-           value = paste0(id, "\n", value)) %>%
+           value = paste0("set linesize 255\ncap graph close\ncap log close\nlog using logfile_blocks/tempsmddoclog", row_number(), ", replace text nomsg\n", value, "\nlog close\ncap graph export picture_blocks/tempsmddocpic", row_number(), ".png, replace width(4800)"),
+           value = paste0(id, "\n", value),
+           value = str_remove_all(value, "\\[\\[ROW_(\\d{1,})\\]\\]--")) %>%
     pull(value) %>%
     writeLines("temp_smd_dofile.do")
   new_md_content <- paste0(md_content, collapse = "\n")
@@ -40,4 +58,10 @@ code_split <- function(file = "main.smd"){
     code_block <- code_blocks[i]
     new_md_content <- str_replace_all(new_md_content, stringr::fixed(code_block), paste0("[[CODE_BLOCK_", i, "]]"))
   }
+  new_md_content %>%
+    str_remove_all("\\[\\[ROW_(\\d{1,})\\]\\]--") -> new_md_content
+  new_md_content %>%
+    write_rds("new_md_content.rds")
+  code_blocks %>%
+    write_rds("code_blocks.rds")
 }
--- R/logfile_handel.R
+++ R/logfile_handel.R
@@ -1,21 +1,29 @@
 #' @title 用于处理 Stata-Markdown 文件编译的后续程序
-#' @param file filename
+#' @param file original filename
+#' @param mdfile new_md_content filename
+#' @param codefile code_blocks filename
 #' @param delete_pic 是否删除中间生成的图片
+#' @param pandoc_dir pandoc_dir
 #' @import stringr
 #' @import dplyr
 #' @import fs
+#' @import rmarkdown
 #' @importFrom rmarkdown render
 #' @importFrom tools file_path_sans_ext
+#' @importFrom readr read_rds
 #' @export
 logfile_handel <- function(file = "main.smd",
-                           delete_pic = T) {
+                           mdfile = "new_md_content.rds",
+                           codefile = "code_blocks.rds",
+                           delete_pic = F,
+                           pandoc_dir = rmarkdown::find_pandoc()$dir) {
   stopifnot(file.exists("logfile_blocks"))
+  stopifnot(file.exists(mdfile))
+  stopifnot(file.exists(codefile))
+  read_rds("new_md_content.rds") -> new_md_content
+  read_rds("code_blocks.rds") -> code_blocks
+  str_remove_all(code_blocks, "\\[\\[ROW_(\\d{1,})\\]\\]--") -> code_blocks
 
-  md_content <- readLines(file)
-  code_blocks <- str_extract_all(paste(md_content, collapse = "\n"), "```[\\s\\S]*?```")[[1]]
-  code_blocks[str_detect(code_blocks, "\\{stata")] -> code_blocks
-
-  new_md_content <- paste0(md_content, collapse = "\n")
   for (i in seq_along(code_blocks)) {
     code_block <- code_blocks[i]
     new_md_content <- str_replace_all(new_md_content, stringr::fixed(code_block), paste0("[[CODE_BLOCK_", i, "]]"))
@@ -35,7 +43,9 @@ logfile_handel <- function(file = "main.smd",
       pull(value) %>%
       writeLines(x)
   }
-
+  # add water mark
+  lapply(fs::dir_ls("picture_blocks", regexp = "png"),
+         rstatatools::addrswm)
   for (i in 1:length(code_blocks)) {
     asis_blocks <- ""
     picture_blocks <- ""
@@ -53,18 +63,27 @@ logfile_handel <- function(file = "main.smd",
     new_md_content <- str_replace(new_md_content, paste0("\\[\\[CODE_BLOCK_", i, "\\]\\]"), paste0(asis_blocks, "\n", logfile_blocks, "\n\n", picture_blocks))
   }
   writeLines(new_md_content, paste0(tools::file_path_sans_ext(basename(file)), ".Rmd"))
-  rmarkdown::render(paste0(tools::file_path_sans_ext(basename(file)), ".Rmd"))
+  try({
+    if (rmarkdown::pandoc_available()) {
+      rmarkdown::render(paste0(tools::file_path_sans_ext(basename(file)), ".Rmd"))
+    }
+    if (!rmarkdown::pandoc_available()) {
+      message("如果没有生成对应的 html 文件，需要使用 RStudio 手动编译！")
+    }
+  })
   if (delete_pic == T) {
     fs::dir_delete("asis_blocks")
     fs::dir_delete("logfile_blocks")
     fs::dir_delete("picture_blocks")
     file.remove("temp_smd_dofile.do")
-    file.remove(paste0(tools::file_path_sans_ext(basename(file)), ".Rmd"))
+    file.remove("new_md_content.rds")
+    file.remove("code_blocks.rds")
   }
   if (delete_pic == F) {
     fs::dir_delete("asis_blocks")
     fs::dir_delete("logfile_blocks")
     file.remove("temp_smd_dofile.do")
-    file.remove(paste0(tools::file_path_sans_ext(basename(file)), ".Rmd"))
+    file.remove("new_md_content.rds")
+    file.remove("code_blocks.rds")
   }
 }
